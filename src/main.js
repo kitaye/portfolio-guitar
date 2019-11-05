0fa6d8e8cd0e8f137e4b3d709f1329966e2cb753
kitaye <n.s.sadovnikov@mail.ru>
2019-11-05 22:07:06 +0300

    added anchorScroll.js

--- src/main.js
+++ src/main.js
@@ -3,4 +3,5 @@ if (process.env.NODE_ENV === "development") {
   require("file-loader!./index.pug");
 }
 
-import "./scripts/skills";
+import anchorScroll from "./scripts/anchorScroll";
+anchorScroll();
---
+++ src/scripts/anchorScroll.js
@@ -0,0 +1,39 @@
+function anchorScroll() {
+    const links = document.querySelectorAll("[href^='#']");
+
+    for (const link of links) {
+        link.addEventListener("click", function (event) {
+            event.preventDefault();
+
+            const attr = link.getAttribute("href");
+            const anchor = document.querySelector(attr);
+            const coordAnchor = anchor.getBoundingClientRect().top;
+            const windowY = window.pageYOffset;
+            let start = null;
+
+            requestAnimationFrame(step);
+
+            function step(time) {
+                if (start === null) {
+                    start = time;
+                }
+
+                const progress = time - start;
+
+                console.log(time, start);
+
+                let coordY =
+                    coordAnchor < 0
+                        ? Math.max(windowY - progress / 0.5, windowY + coordAnchor)
+                        : Math.min(windowY + progress / 0.5, windowY + coordAnchor);
+
+                window.scrollTo(0, coordY);
+                if (coordY !== windowY + coordAnchor) {
+                    requestAnimationFrame(step);
+                }
+            }
+        })
+    }
+}
+
+export default anchorScroll;
